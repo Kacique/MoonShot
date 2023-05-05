b06d464250c0f337b988a14a861510297865427a
Kacique <wbrand90@gmail.com>
2023-05-05 07:34:44 -0700

    "Added images assets and files, completed astronaut struct, "

---
+++ MoonShot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  MoonShot
+//
+//  Created by William on 5/5/23.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable{
+    let id: String
+    let name: String
+    let description: String
+}
